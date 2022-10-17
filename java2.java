3b9df6ec512c981936ac5468365d41f36f564f7b
Ptunc <spolentunc@gmail.com>
2022-10-17 14:52:53 +0300

    just commit

---
+++ java2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class java2{
+    
+
+    public static void main(String[] args) {
+        Double tutar, a = 0.18, b = 0.08, sonuc;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("ürün tutarını girin: ");
+        tutar = input.nextDouble();
+        sonuc = tutar <= 1000 ? a : b;
+        System.out.println("KDVsiz fiyat: " + tutar);
+        System.out.println("KDVli fiyat: " + (tutar + (tutar*sonuc)));
+        System.out.println("KDV tutarı: " + ((tutar + (tutar*sonuc)) - tutar ));
+
+
+    }
+}
